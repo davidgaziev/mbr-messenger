6186a89b1cfb2cfa8aa7d72e9897feb052ca8cef
davidgaziev <davidgaziev6@gmail.com>
2023-11-23 14:15:37 +0600

    migration: add bio to users

---
+++ database/migrations/2023_11_23_013317_add_bio_to_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('bio', 70)->nullable();
+            $table->timestamp('added_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->drop('bio');
+            $table->drop('added_at');
+        });
+    }
+};
